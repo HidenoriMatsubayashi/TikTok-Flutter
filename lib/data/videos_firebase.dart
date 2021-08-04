0ed11d96a13c180776ed64ea352ffb91f1a945a2
HidenoriMatsubayashi <Hidenori.Matsubayashi@sony.com>
2021-08-04 15:53:35 +0900

    Add elinux platform support

--- lib/data/videos_firebase.dart
+++ lib/data/videos_firebase.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tiktok_flutter/data/demo_data.dart';
 import 'package:tiktok_flutter/data/video.dart';
 
@@ -14,29 +13,15 @@ class VideosAPI {
   }
 
   Future<List<Video>> getVideoList() async {
-    var data = await FirebaseFirestore.instance.collection("Videos").get();
-
     var videoList = <Video>[];
     var videos;
 
-    if (data.docs.length == 0) {
-      await addDemoData();
-      videos = (await FirebaseFirestore.instance.collection("Videos").get());
-    } else {
-      videos = data;
-    }
-
-    videos.docs.forEach((element) {
-      Video video = Video.fromJson(element.data());
+    videos = data;
+    videos.forEach((element) {
+      Video video = Video.fromJson(element);
       videoList.add(video);
     });
 
     return videoList;
-  }
-
-  Future<Null> addDemoData() async {
-    for (var video in data) {
-      await FirebaseFirestore.instance.collection("Videos").add(video);
-    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/screens/feed_screen.dart';
 import 'package:tiktok_flutter/service_locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   setup();
   runApp(MaterialApp(
+    scrollBehavior: MyCustomScrollBehavior(),
     debugShowCheckedModeBanner: false,
     home: FeedScreen(),
   ));
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
